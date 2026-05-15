f8c36a6ef8a2eddca243293bead7b40b1202717d
1-2-3-L <mingh6674@gmail.com>
2026-05-15 15:37:09 +0800

    Code"

---
+++ Work/src/com/Work1/lmh/Work1.java
@@ -0,0 +1,14 @@
+package com.Work1.lmh;
+
+import java.util.Scanner;
+
+public class Work1 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //输入数组中元素的个数
+
+
+
+    }
+}
